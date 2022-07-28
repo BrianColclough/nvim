20740a9ba19552d49b8fe8dae7b833c5a3a8c48b
Brian Colclough <brianmcolclough@gmail.com>
2022-07-28 15:56:29 -0400

    added fugitive

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -30,4 +30,5 @@ return require'packer'.startup(function()
     }
     use 'nvim-lua/plenary.nvim'
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use 'tpope/vim-fugitive'
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -124,6 +124,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/briancolclough/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/Users/briancolclough/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
