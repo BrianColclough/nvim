814be2f20b309b7b256c680e51896bcd0027cbfd
Brian Colclough <brianmcolclough@gmail.com>
2022-07-28 18:01:11 -0400

    fixed keybindings and added a lua lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,8 +1,52 @@
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.angularls.setup{}
-require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.rust_analyzer.setup{}
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+ 
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+require'lspconfig'.sumneko_lua.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require'lspconfig'.tsserver.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require'lspconfig'.angularls.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require'lspconfig'.tailwindcss.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require'lspconfig'.rust_analyzer.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 local lsp_defaults = {
   flags = {
     debounce_text_changes = 150,
@@ -22,5 +66,6 @@ lspconfig.util.default_config = vim.tbl_deep_extend(
   lspconfig.util.default_config,
   lsp_defaults
 )
+
 
 
