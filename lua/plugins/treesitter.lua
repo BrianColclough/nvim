ac266a1ae466a79d85d4198900d31c8cdb224fb8
Brian Colclough <brianmcolclough@gmail.com>
2024-03-11 15:14:26 -0400

    adds trouble.lua
    - updates keybindings for telescope

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
 			highlight = {
 				-- `false` will disable the whole extension
 				enable = true,
+                additional_vim_regex_highlighting = { "markdown" },
 			},
 		})
 	end,
