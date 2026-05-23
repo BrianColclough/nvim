f9d6a824ec84b74080053a75d1ec07bbf679a831
Brian Colclough <brianmcolclough@gmail.com>
2026-05-23 14:13:02 -0400

    chore: remove dead commented-out highlight overrides in colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,17 +1,6 @@
 function ColorMyPencils(color)
     color = color or "tokyonight"
     vim.cmd.colorscheme(color)
-
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-    --
-    -- -- Border bg transparency
-    -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
 end
 
 return {
