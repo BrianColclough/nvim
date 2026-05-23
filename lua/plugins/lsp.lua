9d32d59512bfb450a334e643805051dc6aa6e94c
Brian Colclough <brianmcolclough@gmail.com>
2026-05-23 14:08:00 -0400

    fix: add desc to all lsp.lua keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,28 @@
 return {
     "neovim/nvim-lspconfig",
-
     config = function()
-        --mappings
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
-                -- Buffer local mappings.
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
-                local opts = { buffer = ev.buf }
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set("n", "<space>wl", function()
+                local opts = function(desc) return { buffer = ev.buf, desc = desc } end
+
+                vim.keymap.set("n", "gD",          vim.lsp.buf.declaration,                              opts("LSP: go to declaration"))
+                vim.keymap.set("n", "gd",          vim.lsp.buf.definition,                               opts("LSP: go to definition"))
+                vim.keymap.set("n", "K",           vim.lsp.buf.hover,                                    opts("LSP: hover"))
+                vim.keymap.set("n", "gi",          vim.lsp.buf.implementation,                           opts("LSP: go to implementation"))
+                vim.keymap.set("n", "<C-k>",       vim.lsp.buf.signature_help,                           opts("LSP: signature help"))
+                vim.keymap.set("n", "<leader>wa",  vim.lsp.buf.add_workspace_folder,                     opts("LSP: workspace add folder"))
+                vim.keymap.set("n", "<leader>wr",  vim.lsp.buf.remove_workspace_folder,                  opts("LSP: workspace remove folder"))
+                vim.keymap.set("n", "<leader>wl",  function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-                vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "<space>f", function()
-                    vim.lsp.buf.format({ async = true })
-                end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                end, opts("LSP: workspace list folders"))
+                vim.keymap.set("n", "<leader>D",   vim.lsp.buf.type_definition,                          opts("LSP: type definition"))
+                vim.keymap.set("n", "<leader>rn",  vim.lsp.buf.rename,                                   opts("LSP: rename"))
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action,                      opts("LSP: code action"))
+                vim.keymap.set("n", "gr",          vim.lsp.buf.references,                               opts("LSP: references"))
+                vim.keymap.set("n", "<leader>f",   function() vim.lsp.buf.format({ async = true }) end,  opts("LSP: format buffer"))
+                vim.keymap.set("n", "[d",          function() vim.diagnostic.goto_next() end,            opts("LSP: next diagnostic"))
+                vim.keymap.set("n", "]d",          function() vim.diagnostic.goto_prev() end,            opts("LSP: prev diagnostic"))
             end,
         })
     end,
