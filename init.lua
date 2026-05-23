bed233bd1f216a4542423d81f25a3b47d40e0c9f
Brian Colclough <brianmcolclough@gmail.com>
2026-05-23 13:33:06 -0400

    fix: replace deprecated vim.loop with vim.uv

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ autocmd("TextYankPost", {
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
